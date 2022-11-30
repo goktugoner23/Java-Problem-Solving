8ee253c2ed8549e28991e86451e26547a4089811
goktugoner23 <goner@eskisehir.edu.tr>
2022-11-30 03:53:24 +0300

    commit 18
    more probs

--- LeetCode_problems/src/main/java/CalculateNCents.java
+++ LeetCode_problems/src/main/java/CalculateNCents.java
@@ -1,5 +1,16 @@
-/*Given an infinite number of quarters (25 cents), dimes (10 cents), nickels (5 cents) and pennies (1 cent), write code to calculate the number of ways of representing n cents.*/
+/*Given an infinite number of quarters (25 cents), dimes (10 cents), nickels (5 cents) and pennies (1 cent),
+write code to calculate the number of ways of representing n cents.*/
 package main.java;
 
+import java.util.*;
+
 public class CalculateNCents {
+    public static void calculateSets(int number){
+        Set<List<Integer>> sets = new HashSet<>();
+    }
+
+    public static List<Integer> calculateCents(int cent){
+        List<Integer> centList = new ArrayList<>();
+        return centList;
+    }
 }
